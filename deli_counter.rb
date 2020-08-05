b48ab296a3fbbdb768e02920fc3a8feb358c2358
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-05 21:28:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,8 +23,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    return "Currently serving #{katz_deli[0]}."
+    return "Currently serving #{katz_deli.shift}."
   end 
-    return katz_deli.shift
 end 
 
